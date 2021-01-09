0c4de2484e39c70d5c861bcbe23c5785178026de
Bartek <bartlomiejnawoj@gmail.com>
2021-01-09 17:18:10 +0100

    feat: add projects component and context

---
+++ src/components/Projects.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useProjectsValue, useSelectedProjectValue } from "../context";
+
+export const Projects = ({ activeValue = true }) => {
+  const [active, setActive] = useState(activeValue);
+  const { setSelectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+
+  console.log(projects);
+
+  return (
+    projects &&
+    projects.map((project) => (
+      <li
+        key={projects.projectId}
+        data-doc-id={projects.projectId}
+        data-testid="project-action"
+        className={
+          active === projects.projectId
+            ? "active sidebar__project"
+            : "sidebar__project"
+        }
+        onKeyDown={() => {
+          setActive(project.projectId);
+          setSelectedProject(project.projectId);
+        }}
+        onClick={() => {
+          setActive(project.projectId);
+          setSelectedProject(project.projectId);
+        }}
+      >
+        {("Project", JSON.stringify(project))}
+      </li>
+    ))
+  );
+};
