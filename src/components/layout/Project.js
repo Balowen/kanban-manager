ed17b14b0eaae5717b04d82e95192af2e927ca93
Bartek <bartlomiejnawoj@gmail.com>
2021-01-10 22:23:10 +0100

    feat: add project component

---
+++ src/components/layout/Project.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { IoMdTrash } from "react-icons/io";
+import { useProjectsValue, useSelectedProjectValue } from "../../context";
+import { firebase } from "../../firebase";
+
+export const Project = ({ project }) => {
+  const [showConfirm, setShowConfirm] = useState(false);
+  const { projects, setProjects } = useProjectsValue();
+  const { setSelectedProject } = useSelectedProjectValue();
+
+  //   project can be a channel/room in socket.io
+  const deleteProject = (docId) => {
+    firebase
+      .firestore()
+      .collection("projects")
+      .doc(docId)
+      .delete()
+      .then(() => {
+        setProjects([...projects]);
+        setSelectedProject("today");
+      });
+  };
+
+  return (
+    <>
+      <span className="sidebar__bullet">•</span>
+      <span className="sidebar__project-name">{project.name}</span>
+      <span
+        className="sidebar__project-delete"
+        data-testid="delete-project"
+        onClick={() => setShowConfirm(!showConfirm)}
+      >
+        <IoMdTrash />
+        {showConfirm && (
+          <div className="project-delete-modal">
+            <div className="project-delete-modal__inner">
+              <p>Czy na pewno chcesz usunąć ten projekt?</p>
+              <button
+                type="button"
+                onClick={() => deleteProject(project.docId)}
+              >
+                Usuń
+                <span onClick={() => setShowConfirm(!showConfirm)}>Anuluj</span>
+              </button>
+            </div>
+          </div>
+        )}
+      </span>
+    </>
+  );
+};
